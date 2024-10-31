a23ae756e75ca5ef7b02b81d8a53ae89e3037826
Ünal Hazar <unalhazar40@gmail.com>
2024-10-31 11:55:44 +0300

    .

---
+++ Services/HAZAR.AUTH.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HAZAR.AUTH.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
